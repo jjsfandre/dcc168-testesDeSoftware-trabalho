b3ff7c72e3c995cc76c9e28304f3c2414a5c759f
Jônatas André <jjsfandre@gmail.com>
2019-11-24 20:31:16 -0300

    Subindo projeto sem os testes do JUnit

---
+++ PongGame-master/PongGame/src/pong/main/tests/TestMain.java
@@ -0,0 +1,18 @@
+package pong.main.tests;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+
+	@Test 
+	void testMain3() {
+		String entrada= "4$";
+		int retornoEsperado=0;
+		int retornoObtido = 0;
+		assertEquals(retornoEsperado, retornoObtido);
+	}
+	
+}
