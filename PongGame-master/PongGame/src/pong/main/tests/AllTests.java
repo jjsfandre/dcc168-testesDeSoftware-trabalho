4fd9209c0830a20fad31f1f3f61970fe3bb5e4dd
Jônatas André <jjsfandre@gmail.com>
2019-11-29 00:07:29 -0300

    Incluindo mais testes da tela do menu principal

--- PongGame-master/PongGame/src/pong/main/tests/AllTests.java
+++ PongGame-master/PongGame/src/pong/main/tests/AllTests.java
@@ -2,13 +2,13 @@ package pong.main.tests;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import pong.game.Game;
+import pong.states.FPSState;
 import pong.states.GameState;
 import pong.states.HelpState;
 import pong.states.MenuState;
@@ -19,22 +19,12 @@ import pong.states.StateManager;
 public class AllTests {
 	
 	private Game game;
-	private static final int EXIT_CODE = 5;
-	
-	//@Rule
-    //public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 	
 	public AllTests() {
 
 		game = new Game(false);
 		game.start();
 	}
-
-	@Before
-    public void setUp() {
-		//game = new Game();
-		//game.start();
-    } 
 
 	@Test 
 	void start_enter_iniciarJogo() {
@@ -58,7 +48,7 @@ public class AllTests {
 		assertTrue("Tela de ajuda não foi acessada corretamente",sm.getState() instanceof HelpState);
 	}
 	@Test 
-	void start_enter_sairDoJogo() throws Exception{
+	void start_enter_sairDoJogo() {
 		StateManager sm = game.getStateManager();
 		StateManager.setState(StateManager.MENU);
 		MenuState currentState = (MenuState)sm.getState();
@@ -66,6 +56,45 @@ public class AllTests {
 		currentState.checkKeyPressed(10);	
 		
 		assertTrue("Jogo não foi fechado corretamente",!game.isRunning());
-	}	
+	}
+	@Test 
+	void esc_nadaAFazer(){
+		StateManager sm = game.getStateManager();
+		StateManager.setState(StateManager.MENU);
+		MenuState currentState = (MenuState)sm.getState();
+		currentState.checkKeyPressed(27);	
+		
+		assertTrue("O estado não deve ser alterado ao clicar em esc",sm.getState() instanceof MenuState);
+	}
+	
+	@Test 
+	void teclasDiversas_nadaAFazer(){
+		boolean error = false;
+		StateManager sm = game.getStateManager();
+		StateManager.setState(StateManager.MENU);
+		MenuState currentState = (MenuState)sm.getState();
+		for(int tecla = 0; tecla<256;tecla++) {
+			if (tecla==10 || tecla == 70)
+				continue;
+			currentState.checkKeyPressed(tecla);
+			if (!(sm.getState() instanceof MenuState)) {
+				error = true;
+				break;
+			}
+		}
+		assertTrue("O estado não deve ser alterado ao clicar em qualquer tecla exceto o Enter",!error);
+	}
+	
+	@Test 
+	void f_fpsMode(){
+		StateManager sm = game.getStateManager();
+		StateManager.setState(StateManager.MENU);
+		MenuState currentState = (MenuState)sm.getState();
+		currentState.checkKeyPressed(70);	
+		
+		assertTrue("Ao digitar F deve entrar em FPSMode",sm.getState() instanceof FPSState);
+	}
+	
+	
 	
 }
